9a97261fb8cd24b74b9317198b6e5df816612bc4
JuanAguirre10 <juancho71096@gmail.com>
2026-04-25 11:23:21 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+import connectDB from "./src/db/database.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// rutas
+import homeRoutes from "./src/routes/home.routes.js";
+import postRoutes from "./src/routes/post.routes.js";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src", "views"));
+
+// middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "src", "public")));
+
+// rutas
+app.use("/", homeRoutes);
+app.use("/posts", postRoutes);
+
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
